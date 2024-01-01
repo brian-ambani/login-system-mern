ee3281f641326d86b262d7d9f2778dd6ab1e4a5a
brian-ambani <brainambani1@gmail.com>
2024-01-01 17:20:59 +0300

    updated the login link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <div>Hello React Login system</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
