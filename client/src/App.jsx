88b23e6a93893708fbc17ea402a2818d1eecbd97
brian-ambani <brainambani1@gmail.com>
2024-01-01 17:35:47 +0300

    updated the docs and pricing links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DocsPage from "./pages/DocsPage";
+import PricingPage from "./pages/PricingPage";
+
 function App() {
   return (
     <Router>
@@ -10,6 +13,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/docs" element={<DocsPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
       </Routes>
     </Router>
   );
